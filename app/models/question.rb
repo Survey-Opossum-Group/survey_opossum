76109dbd1c9467fb28c1421f711ed7c1e39c9d6b
Scott Williams <williamsjs1224@gmail.com>
2015-06-13 12:47:42 -0400

    nest questions inside survey from

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,6 @@ class Question < ActiveRecord::Base
   has_many :answers
   belongs_to :survey
 
-  validates_presence_of :survey
   validates :survey_id, presence: true
 
 end
--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,8 +1,8 @@
 class Survey < ActiveRecord::Base
   belongs_to :author
 
-  has_many :questions
-  accepts_nested_attributes_for :questions, reject_if: :all_blank, allow_destroy: true 
+  has_many :questions, dependent: :destroy
+  accepts_nested_attributes_for :questions, reject_if: :all_blank, allow_destroy: true
 
   validates :name, :author_id, :description, presence: true
 
