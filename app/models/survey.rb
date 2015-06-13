36303e7db6a145ca5ea69ac4a1decacff57a59bf
Scott Williams <williamsjs1224@gmail.com>
2015-06-13 14:58:11 -0400

    f

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -2,7 +2,7 @@ class Survey < ActiveRecord::Base
   belongs_to :author
 
   has_many :questions, dependent: :destroy
-  accepts_nested_attributes_for :questions, reject_if: :all_blank, allow_destroy: true
+  accepts_nested_attributes_for :questions, :reject_if => :all_blank, :allow_destroy => true, allow_destroy: true
 
   validates :name, :author_id, :description, presence: true
 
